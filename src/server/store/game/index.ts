4b6b0ea31ecc5abcee4c95a674b9a9809cc13ab3
Thomas Scheuneman <tscheuneman@users.noreply.github.com>
2022-08-02 23:44:06 -0700

    Add node types and last update state

--- src/server/store/game/index.ts
+++ src/server/store/game/index.ts
@@ -6,12 +6,13 @@ interface UserStatus {
 export interface GameInterface {
     status: 'busy' | 'complete',
     state: UserStatus[],
+    lastUpdate: number,
 }
 
 export class Game {
     private game: GameInterface;
     constructor(creator: UserStatus) {
         console.log('item');
-        this.game = { state: [creator], status: 'busy' };
+        this.game = { state: [creator], status: 'busy', lastUpdate: Date.now() };
     }
 }
